ade6c5585cc2cd76c33b396d19fbd24e769b6911
ferreirarauldev <ferreirarauldev@gmail.com>
2021-02-26 17:00:02 -0300

    init

--- reloj_checador_team03/app/controllers/reports_controller.rb
+++ reloj_checador_team03/app/controllers/reports_controller.rb
@@ -1,11 +1,13 @@
 class ReportsController < ApplicationController
 
     def attendance_by_day
+        @attendances = Attendance.attendance_by_day()
     end
 
     def average_time_employees
     end
 
     def absence_employee
+       
     end
 end
--- reloj_checador_team03/app/models/attendance.rb
+++ reloj_checador_team03/app/models/attendance.rb
@@ -1,3 +1,8 @@
 class Attendance < ApplicationRecord
   belongs_to :employee
+
+
+  def self.attendance_by_day(date = Date.today)
+    Attendance.where(date_time: date.all_day).all
+  end
 end
